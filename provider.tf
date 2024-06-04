f216dd9282aa0c91338e28301820ed8b37d8752c
OAKtocracy <124656072+OAKtocracy@users.noreply.github.com>
2024-06-03 21:56:55 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,1 @@
-terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
 
- backend "s3" {
-   bucket = "tf-state"
-   key    = "state"
-   region = "us-east-1"
- }
-}
